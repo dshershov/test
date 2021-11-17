346d3ac1fcde37de70d92490646fa3993f422b8c
Denis Shershov <commandos_95@mail.ru>
2021-11-17 16:06:49 +0400

    small fix for py script

--- churn-predictions.py
+++ churn-predictions.py
@@ -197,5 +197,5 @@ if __name__ == '__main__':
         model_uri = os.path.join(artifact_uri, Defaults.mlflow_experiment_name)
         log.info(f'Run id = {run_id}; artifact_uri = {artifact_uri}; experimentId = {experiment.experiment_id}')
         # create_bucket(Defaults.sagemaker_bucket, Defaults.AWS_region)
-        if os.getenv('USE_SAGEMAKER_ENDPOINT') and os.getenv('USE_SAGEMAKER_ENDPOINT').lower() == 'true':
+        if os.getenv('USE_KUBERNETES_MODEL') and os.getenv('USE_KUBERNETES_MODEL').lower() == 'Disabled':
             deploy(model_uri)
